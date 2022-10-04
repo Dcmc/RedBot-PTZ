0ff76bc1b4f8068049c8f18ea4528495d73b3221
DcMc <davis.mccoy77@gmail.com>
2022-10-03 19:24:01 -0500

    fixed init things

--- PTZ.py
+++ PTZ.py
@@ -9,7 +9,12 @@ from redbot.core import commands
 
 log = logging.getLogger("red.cbd-cogs.tube")
 
-class PTZ(commands.Cog): 
+class PTZ(commands.Cog):
+	"""My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+	
 	@commands.command()
 	async def lookUp():
 		requests.get('http://10.10.0.19:8888/press/bank/1/3')
